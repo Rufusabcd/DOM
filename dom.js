ab4a761073120de76424e672ce9cab8697060beb
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-28 20:55:54 +0200

    innerHTML

--- dom.js
+++ dom.js
@@ -1,4 +1,6 @@
 var getButton = document.getElementsByClassName('button')
 for (var i = 0; i < getButton.length ; i++) {
-    
+    var text = document.createElement('p');
+    text.innerHTML = 'To jest button';
+    console.log(text)
 }
