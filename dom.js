7485b98c447715ac7301aba984ed73d6963f1036
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-29 00:23:02 +0200

    btnchanges

--- dom.js
+++ dom.js
@@ -1,6 +1,7 @@
 var getButton = document.getElementsByClassName('button')
 for (var i = 0; i < getButton.length ; i++) {
-    var text = document.createElement('p');
-    text.innerHTML = 'To jest button';
-    console.log(text)
+    var navigation = document.getElementById('btn');
+    console.log(navigation.nextElementSibling);
+    navigation.innerHTML = 'To jest button';
+    console.log(navigation)
 }
