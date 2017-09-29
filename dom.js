1a055e0cc3fead79e7c9924d735dd17f4dce9993
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-29 08:00:22 +0200

    changes2

--- dom.js
+++ dom.js
@@ -1,7 +1,6 @@
 var getButton = document.getElementsByClassName('button')
 for (var i = 0; i < getButton.length ; i++) {
-    var navigation = document.getElementById('btn');
-    console.log(navigation.nextElementSibling);
-    navigation.innerHTML = 'To jest button';
-    console.log(navigation)
+    console.log(getButton.indexOf(getButton[i]))
+    getButton.innerHTML = 'To jest button';
+    console.log(getButton)
 }
