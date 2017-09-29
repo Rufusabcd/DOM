0c447e688bb9760e73a55f69aa21e91e217ed7a5
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-09-29 18:19:03 +0200

    domchanges

--- dom.js
+++ dom.js
@@ -1,6 +1,4 @@
 var getButton = document.getElementsByClassName('button')
 for (var i = 0; i < getButton.length ; i++) {
-    console.log(getButton.indexOf(getButton[i]))
-    getButton.innerHTML = 'To jest button';
-    console.log(getButton)
+    getButton.innerHTML = "To jest przycisk"
 }
